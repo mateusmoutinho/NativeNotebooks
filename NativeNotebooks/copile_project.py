4d776e1eaac3b6ed9ee394b3a63d5a55afceccf3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-01 15:22:48 -0300

    at

--- NativeNotebooks/copile_project.py
+++ NativeNotebooks/copile_project.py
@@ -12,12 +12,29 @@ def copile_file(file_content:str,lang:str)->set:
     return formated
 
 def copile_project(current_dir:str, lang:str,start_flag:str,out_dir:str):
+    
 
-    all  = create_tree_from_hardware(current_dir,load_content=False,preserve_content=False)
- 
+    all  = create_tree_from_hardware(
+        current_dir,
+        load_content=False,
+        preserve_content=False
+    )
+    
+    formated_tree = []
+
+    if out_dir !='.':
+        tree_to_remove = create_tree_from_hardware(
+            out_dir,
+            load_content=False,
+            preserve_content=False
+        )
+        for file in tree_to_remove:
+            file.hardware_remove()
+            formated_tree.append(file)
+
+    #print("current dir is",current_dir)
     for file in all:
-   
-        dir = file.path.get_dir()
+        
         extension = file.path.get_extension()
         if extension == 'ipynb':
             file.load_content_from_hardware()
@@ -27,9 +44,32 @@ def copile_project(current_dir:str, lang:str,start_flag:str,out_dir:str):
             name = file.path.get_name()
             file.path.set_name(start_flag+name)
             file.hardware_write()
+           
+                    
         if out_dir !='.':
-            file.path.set_dir(out_dir)
+            
+            file.load_content_from_hardware()
+            origina_path = file.path.get_path()
+            
+
+
+            path = origina_path.replace(current_dir,out_dir +'/')
+            file.path.set_path(path)
+      
+            if out_dir in origina_path:
+                continue    
+            if 'notebook.yaml' in path:
+                continue
+            if '.vscode' in path:
+                continue
+            
             file.hardware_write()
+        
+        formated_tree.append(file)
     
-    
-    hardware_commit_tree(all)
+
+    t = create_transaction_report(formated_tree)
+    t.represent()
+    procede = input("procede? (y/n)")
+    if procede == 'y':
+        hardware_commit_tree(all)
--- teste.py
+++ teste.py
@@ -2,4 +2,7 @@ from os import system
 from NativeNotebooks.copile_project import copile_project
 from NativeNotebooks.exec import main
 
-main()
+from PyDoTheWorld import *
+
+r = get_current_dir()
+print(r)
