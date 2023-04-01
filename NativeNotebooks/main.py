f5607c8551361f3f11c8f71078e0a8cbd5202422
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-01 07:04:40 -0300

    at

--- NativeNotebooks/main.py
+++ NativeNotebooks/main.py
@@ -1,7 +1,6 @@
 from PyDoTheWorld import * 
 from cli_args_system import *
-from os.path import abspath,dirname
-
+from NativeNotebooks.create_template import *
 def main():
     args = Args()
     
@@ -9,17 +8,7 @@ def main():
     
 
     if new_project.exist():
-        print('Creating new project')
-        path = dirname(abspath(__file__))
-        templates = f'{path}/template'
-        current_dir = get_current_dir() + '/'
-        if new_project.filled():
-            name = new_project._args[0]
-            current_dir += name
-        tree = create_tree_from_hardware(templates)
-        for file in tree:
-            file.path.set_dir(current_dir)
-            file.hardware_write()
-            
-        hardware_commit_tree(tree)
+        create_template(new_project)
         return 
+
+    
