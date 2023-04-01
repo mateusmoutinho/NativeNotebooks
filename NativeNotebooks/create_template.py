bab6ea90392ffb357d24bc232132f98797ea0105
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-01 12:28:09 -0300

    at

--- NativeNotebooks/create_template.py
+++ NativeNotebooks/create_template.py
@@ -65,5 +65,24 @@ def create_template():
         c.hardware_write()
         tree.append(c)
 
+    #ading notebooks
+    notebooks_path = join(templates_path,'notebooks')
+    
+    lang_path = join(notebooks_path,informations['$mainLang$'])
+    notebooks_tree = create_tree_from_hardware(lang_path)
+    for c in notebooks_tree:
+        c_path = c.path.get_path()
+        new_cdir_path = c_path.replace(lang_path ,'')
+        concated_path =  code_dir  + new_cdir_path
+        c.path.set_path(concated_path)
+        if c.in_memory():
+            content = c.get_content()
+            for key,value in informations.items():
+                content  = content.replace(key,value)
+            c.set_content(content)    
+
+        c.hardware_write()
+        tree.append(c)
+    
 
     hardware_commit_tree(tree)
