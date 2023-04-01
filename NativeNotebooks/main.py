2da84145c1d7bc41a3a4dd445b5876e0d359a626
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-01 06:14:31 -0300

    at

--- NativeNotebooks/main.py
+++ NativeNotebooks/main.py
@@ -7,17 +7,14 @@ def main():
     
     new_project = args.flags_content('new')
     
-    if new_project.exist_and_empty():
-        print('no folder provided')
-        return
+
     if new_project.exist():
         path = dirname(abspath(__file__))
         templates = f'{path}/template'
-        name = new_project.flags()[0]
         tree = create_tree_from_hardware(templates)
         for file in tree:
             dir = file.path.get_dir()
-            new_path = dir.replace('template', name)
+            new_path = dir.replace('template', '')
             
             file.path.set_dir(new_path)
             file.hardware_write()
