dc27de95b8d04f512409cf47fa30cdf6ec8385d6
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-01 06:58:42 -0300

    add folder suport

--- NativeNotebooks/main.py
+++ NativeNotebooks/main.py
@@ -13,6 +13,9 @@ def main():
         path = dirname(abspath(__file__))
         templates = f'{path}/template'
         current_dir = get_current_dir() + '/'
+        if new_project.filled():
+            name = new_project._args[0]
+            current_dir += name
         tree = create_tree_from_hardware(templates)
         for file in tree:
             file.path.set_dir(current_dir)
