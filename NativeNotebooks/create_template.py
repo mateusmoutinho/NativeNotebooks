29925c4bd48ffeab6293baea4c55a364a9fe1c69
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-01 07:34:02 -0300

    at

--- NativeNotebooks/create_template.py
+++ NativeNotebooks/create_template.py
@@ -8,16 +8,12 @@ from os.path import abspath,dirname
 def create_template(new_project:FlagsContent):
     print('Creating new project')
     path = dirname(abspath(__file__))
-    templates = f'{path}/template'
+    templates = f'{path}/NativeNotebookTemplate'
+
     current_dir = get_current_dir() + '/'
         
     if new_project.filled():
         name = new_project._args[0]
         current_dir = name
-        
-    tree = create_tree_from_hardware(templates)
-    for file in tree:
-        file.path.set_dir(current_dir)
-        file.hardware_write()
-        
-    hardware_commit_tree(tree)
+    
+    copy_any(templates,current_dir,True)
